fbfa0ba88deb167b506a62c7735cbcda96d7f0d8
chungtamz1801 <chungtam1801@gmail.com>
2025-12-11 16:04:12 +0700

    init_project

---
+++ app/src/main/java/com/example/hoctap_kiemtra_app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hoctap_kiemtra_app;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        testFireBase();
+    }
+    void testFireBase(){
+        var db = FirebaseFirestore.getInstance();
+        var user = new HashMap<String,String>();
+        user.put("sdf","egag");
+        // Add a new document with a generated ID
+        db.collection("users").add(user);
+    }
+}
